b28ce958a7d0848562a7b98ed9d60489a40a777a
atok624 <84607674+atok624@users.noreply.github.com>
2023-04-21 20:58:38 +0000

    Make changes to style

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,45 +1,20 @@
 import '../styles/MainPage.css';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-  faArrowLeft,
-  faSearch,
-  faVirusCovid,
   faArrowRight,
-  faGlobe,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useNavigate } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import PropTypes from 'prop-types';
 
 import { useDispatch, useSelector } from 'react-redux';
 import GetData from '../redux/Home-Api/HomeAPI';
-import Component from './Details';
-import { setDetails } from '../redux/Home-Api/HomeSlice';
-
-const Info = ({ confirmed, deaths }) => (
-  <div className="Info">
-    <h2 className="global">
-      GLOBAL COVID CASES
-      <br />
-      (2020 - PRESENT)
-    </h2>
-    <div className="Info-Wrapper">
-      <span className="span">
-        <p className="confirmed-text-home">CONFIRMED</p>
-        <p className="confirmed">{confirmed}</p>
-      </span>
-      <span>
-        <p className="deaths-text">DEATHS</p>
-        <p className="deaths">{deaths}</p>
-      </span>
-    </div>
-  </div>
-);
 
 const Section = ({ country, cases }) => {
-  const dispatch = useDispatch();
+  const navigate = useNavigate();
   function handleClick() {
-    dispatch(setDetails(country));
+    navigate(`details/${country}`);
   }
   return (
     <button type="button" className="section" onClick={() => handleClick()}>
@@ -56,9 +31,8 @@ const Section = ({ country, cases }) => {
 
 const Home = () => {
   const dispatch = useDispatch();
-  const { data, state, details } = useSelector((store) => store.Home);
-  const [searchData, setSearchData] = useState('');
-  const arrowBack = useRef(null);
+  const { data, state } = useSelector((store) => store.Home);
+  const [searchData] = useState('');
 
   useEffect(() => {
     if (data.length < 1) {
@@ -66,13 +40,7 @@ const Home = () => {
     }
   }, [dispatch, data]);
 
-  function handleArrowBack() {
-    dispatch(setDetails(null));
-    arrowBack.current.style.display = 'none';
-  }
-
   let content;
-
   if (state === 'Success') {
     content = (
       <div className="second-row">
@@ -90,74 +58,13 @@ const Home = () => {
       </div>
     );
   }
+  console.log(searchData);
 
-  if (state === 'Success' && details !== null && arrowBack.current !== null) {
-    arrowBack.current.style.display = 'block';
-    const indexOfData = [
-      data.rawData.findIndex((obj) => obj.Combined_Key === details),
-    ];
-    content = (
-      <div className="details-row">
-        <Component
-          date={data.rawData[indexOfData].Last_Update}
-          country={data.rawData[indexOfData].Combined_Key}
-          incident={parseFloat(data.rawData[indexOfData].Incident_Rate).toFixed(
-            4,
-          )}
-          cases={data.rawData[indexOfData].Confirmed}
-          deaths={data.rawData[indexOfData].Deaths}
-          ratio={parseFloat(
-            data.rawData[indexOfData].Case_Fatality_Ratio,
-          ).toFixed(4)}
-        />
-      </div>
-    );
-  }
   return (
     <div className="home">
-      <nav className="navbar">
-        <i
-          ref={arrowBack}
-          role="button"
-          tabIndex={0}
-          className="goBack"
-          onClick={handleArrowBack}
-          onKeyDown={handleArrowBack}
-        >
-          <FontAwesomeIcon icon={faArrowLeft} />
-        </i>
-        <p className="covid-19-report">
-          <FontAwesomeIcon icon={faVirusCovid} />
-          COVID-19 CDR
-        </p>
-        <span className="searchBar">
-          <input
-            type="text"
-            value={searchData}
-            onChange={(e) => setSearchData(e.target.value)}
-            placeholder="Search Country"
-          />
-          <i className="search-icon">
-            <FontAwesomeIcon icon={faSearch} />
-          </i>
-        </span>
-        <p className="second covid-19-report">
-          <FontAwesomeIcon icon={faGlobe} />
-          GLOBAL CASES
-        </p>
-      </nav>
-      <Info
-        confirmed={state === 'Success' ? data.summaryStats.global.confirmed : 0}
-        deaths={state === 'Success' ? data.summaryStats.global.deaths : 0}
-      />
       {content}
     </div>
   );
-};
-
-Info.propTypes = {
-  confirmed: PropTypes.number.isRequired,
-  deaths: PropTypes.number.isRequired,
 };
 
 Section.propTypes = {
